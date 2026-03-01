2ea45e2afa71e8b4f279a8c85d34f1aaea48fbf9
geoproduct <12181187@inha.edu>
2026-03-01 23:51:56 +0900

    fix: kakao scope order

--- routes/auth.py
+++ routes/auth.py
@@ -33,7 +33,7 @@ def init_oauth(app):
             access_token_url='https://kauth.kakao.com/oauth/token',
             authorize_url='https://kauth.kakao.com/oauth/authorize',
             api_base_url='https://kapi.kakao.com',
-            client_kwargs={'scope': 'account_email profile_nickname'},
+            client_kwargs={'scope': 'profile_nickname account_email'},
         )
 
 
